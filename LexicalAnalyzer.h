780fce43e5b4629dd7d5f12acc12ff0de87971db
Nocturnal Light <deividdo-chan@hotmail.com>
2017-12-23 03:09:20 -0800

    initial

---
+++ LexicalAnalyzer.h
@@ -0,0 +1,76 @@
+/*
+LexicalAnalyzer.h
+
+CPSC 323, Spring 2016, Prof. Song Choi, Assignment 1
+
+NAME:			David Tu, Section 2 (david.tu2@csu.fullerton.edu)
+DESCRIPTION:	Rat16 Compiler - Lexical Analyzer implementation (Assignment 1)
+REVISIONS:		Version 1
+MODIFIED BY:	2016/03/06
+*/
+
+#pragma once
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+using namespace std;
+
+class LexicalAnalyzer
+{
+public:
+	//constructor
+	LexicalAnalyzer();
+
+	//destructor
+	~LexicalAnalyzer();
+
+	//getters
+	bool readFileOpen();
+	string getToken();
+	string getLexeme();
+
+	//setters
+	void closeReadFile();
+	void closeOutputFile();
+
+	//other functions
+	//output a token and corresponding lexeme into the output file
+	void lexer();
+	//accumulates more alpha and _ characters into a variable called lexeme and correspondingly updates the lexState variable
+	void runIDDFSM();
+	//accumulates more digit and decimal characters into a variable called lexeme and correspondingly updates the lexState variable
+	void runIntRealDFSM();
+	//takes a lexeme and checks against the "vectorToCheck" for any keywords, separators or operators - depending on what I pass to "vectorToCheck"
+	//If there's a match, the function returns true
+	bool vectorChecker(vector<string>& vectorToCheck);
+	//recursive algoritm which skips the comment block. The algoritm terminates once the string, twoCharString matches with "*]"
+	void commentSkip(const string twoCharString);
+
+private:
+	//holds the lexeme after lexer() is completed
+	string lexeme;
+	//container for get/unget functions
+	char ch;
+	//container for column assignment based on the value of ch
+	int column;
+	//holds the token after lexer() is completed
+	string token;
+	//container for lexical state assignment based on the value of ch and column
+	int lexState;
+	//Input file - Test Cases are read from here
+	ifstream myReadFile;
+	//Output file - output of Test Case are placed here
+	ofstream myOutputFile;
+	//intRealDFSM (Integer/Real DFSM)
+	vector< vector<int> > intRealDFSM;
+	//IDDFSM (Identifier DFSM)
+	vector< vector<int> > IDDFSM;
+	//Keywords:
+	vector <string> keywords;
+	//Separators:
+	vector <string> separators;
+	//Operators:
+	vector <string> operators;
+};
---
+++ Project 1 Lexer.cpp
@@ -0,0 +1,31 @@
+/*
+Project 1 Lexer.cpp
+
+CPSC 323, Spring 2016, Prof. Song Choi, Assignment 1
+
+NAME:			David Tu, Section 2 (david.tu2@csu.fullerton.edu)
+DESCRIPTION:	Rat16 Compiler - Lexical Analyzer implementation (Assignment 1)
+REVISIONS:		Version 1
+MODIFIED BY:	2016/03/06
+*/
+
+#include "stdafx.h"
+#include "LexicalAnalyzer.h"
+
+LexicalAnalyzer NewLA;
+
+int main()
+{
+	if (NewLA.readFileOpen()) {
+		NewLA.lexer();
+	}
+	else {
+		cout << "ERROR: Cannot open the file" << endl;
+		exit(1);
+	}
+
+	NewLA.closeReadFile();
+	NewLA.closeOutputFile();
+
+	return 0;
+}
